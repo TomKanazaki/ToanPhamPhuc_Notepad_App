bdd91a3fa992c860a2d4304aaab1abd686044b37
Pham Phuc Toan <toanpham31803@gmaill.com>
2024-08-28 20:06:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/toanphamphuc_notepad_app/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.toanphamphuc_notepad_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.toanphamphuc_notepad_app.ui.theme.ToanPhamPhuc_Notepad_AppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ToanPhamPhuc_Notepad_AppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ToanPhamPhuc_Notepad_AppTheme {
+        Greeting("Android")
+    }
+}
